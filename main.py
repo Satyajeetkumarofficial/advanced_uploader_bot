2c4ed53b36a2587a5b9b5be8f33870a69af59313
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-11-26 17:01:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ from handlers.start import register_start_handlers
 from handlers.user_settings import register_user_settings_handlers
 from handlers.admin import register_admin_handlers
 from handlers.url_handler import register_url_handlers
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive", 200
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 
 def main():
